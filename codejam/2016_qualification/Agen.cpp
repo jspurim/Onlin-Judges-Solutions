f30bb0ab15c49c74b906495104792613d009ec9a
Jerónimo Spuri Murguía <jspurim@gmail.com>
2016-04-09 22:46:37 -0300

    Code jam qualification 2016

---
+++ codejam/2016_qualification/Agen.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#define DB(x) cerr<<#x<<": "<<x<<" ";
+#define DBL(x) cerr<<#x<<": "<<x<<endl;
+#else
+#define DB(x)
+#define DBL(x)
+#endif
+
+#define ANS(n, r) cout<<"Case #"<<n<<": "<<r<<endl
+
+int main(){
+  cout<<1000001<<endl;
+  for(int i=0;i<=1000000;i++)
+    cout<<i<<endl;
+  return 0;
+}
