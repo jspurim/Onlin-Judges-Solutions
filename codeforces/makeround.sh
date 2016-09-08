c3d2245525d1604dd8cc491e30776d1aad452a11
Jerónimo Spuri Murguía <jspurim@gmail.com>
2016-09-08 08:28:19 -0300

    Add CF scripts.

---
+++ codeforces/makeround.sh
@@ -0,0 +1,7 @@
+DIR="round$1_div2"
+mkdir $DIR
+cp ./model.cpp $DIR/A.cpp
+cp ./model.cpp $DIR/B.cpp
+cp ./model.cpp $DIR/C.cpp
+cp ./model.cpp $DIR/D.cpp
+cp ./model.cpp $DIR/E.cpp
---
+++ codeforces/model.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG
+#define DB(x) cerr<<#x<<": "<<x<<" ";
+#define DBL(x) cerr<<#x<<": "<<x<<endl;
+#else
+#define DB(x)
+#define DBL(x)
+#endif
+int main(){
+  
+  return 0;
+}
